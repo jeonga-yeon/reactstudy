ae3bc3f7227f13640390e5c49bea02fc234e9826
joyfuldev <wings2132@naver.com>
2023-05-05 19:30:41 +0900

    Add: 디테일 페이지 완료

--- src/pages/Monster/Monster.js
+++ src/pages/Monster/Monster.js
@@ -1,19 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../../components/Card/Card";
 import "./Monster.scss";
+import { useNavigate, useParams } from "react-router-dom";
 
 function Monster() {
   const [monster, setMonster] = useState({});
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((response) => response.json())
+      .then((data) => setMonster(data));
+  }, []);
+
+  const numId = Number(id);
+
+  const handlePrev = () => {
+    if (numId < 2) return;
+    navigate(`/detail/${numId - 1}`);
+  };
 
   return (
     <article className="monster">
       <div className="btnWrapper">
-        <button>Back to Monsters List</button>
+        <button onClick={() => navigate("/")}>Back to Monsters List</button>
       </div>
-      <Card />
+      <Card id={id} name={monster.name} email={monster.email} />
       <div className="btnWrapper">
-        <button>Previous</button>
-        <button>Next</button>
+        <button onClick={handlePrev}>Previous</button>
+        <button onClick={() => navigate(`/detail/${numId + 1}`)}>Next</button>
       </div>
     </article>
   );
